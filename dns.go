112d98cebd350ba87f5c8df66e3e12e1bbcb9e66
Harsh Nagaonkar <harshu+github@gmail.com>
2019-01-06 20:46:06 -0800

    Added stubs for api endpoints.

--- dns.go
+++ dns.go
@@ -21,18 +21,44 @@ type Zone struct {
 	Record []Record `json:record`
 }
 
-func main() {
+var records []Record
+var zones []Zone
 
+func main() {
 	fmt.Printf("hello")
+
+	db, err := sql.Open("sqlite3", "./dns.db")
+	if err != nil {
+		log.Errorf("unable to create sqlite database: %v", err)
+	}
+	// Insert snippet here to handle JSON -> DB conversion
+	// for zones and records relationships
+
+	// This defines the operations we want in the api,
+	// namely creating/deleting zones and
+	// view and manage all of the records in those zones
 	router := mux.NewRouter()
 	router.HandleFunc("/record", GetRecord).Methods("GET")
 	router.HandleFunc("/zone", GetZone).Methods("GET")
 	router.HandleFunc("/record/{name}", SetRecord).Methods("POST")
 	router.HandleFunc("/zone/{name}", DeleteZone).Methods("DELETE")
+
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func GetRecord(w http.ResponseWriter, r *http.Request) {}
-func GetZone(w http.ResponseWriter, r *http.Request)   {}
-func SetRecord(w http.ResponseWriter, r *http.Request) {}
-func DeleteZone(w http.ResponseWriter, r *http.Request)   {}
+// Validate that Record Type is either A/CNAME
+// For A, validate that the data is an IPV4 address
+// For CNAME, validate that the data is a valid domain name
+// For Record Name, validate either root domain if @ present or subdomain
+func validateRecord() {
+}
+
+// Validate that the name is a valid domain name
+func validateZone() {
+}
+
+// All of these should have DB commits at the end of each operation
+func GetRecord(w http.ResponseWriter, r *http.Request)  {}
+func GetZone(w http.ResponseWriter, r *http.Request)    {}
+func SetRecord(w http.ResponseWriter, r *http.Request)  {}
+func DeleteZone(w http.ResponseWriter, r *http.Request) {}
